f19176b4a4e0076fb80fce9cfcbc1200bb14c2df
chaosssliu <liushashiwr@gmail.com>
2017-11-12 16:06:32 -0500

    Update S3Config.java

--- UploadFile/src/main/java/com/antra/uploadfile/config/S3Config.java
+++ UploadFile/src/main/java/com/antra/uploadfile/config/S3Config.java
@@ -9,9 +9,9 @@ import com.amazonaws.services.s3.AmazonS3Client;
 @Configuration
 public class S3Config {
 
-	private String accessId = "AKIAJHYCP4L3IFBL2SKQ";
+	private String accessId = "accessId";
 	
-	private String accessKey = "Dc9JZzMKSSmuYMUVqQL6HicAIth5A8g7tV9eF0bw";
+	private String accessKey = "accessKey";
 	
 	@Bean
 	public BasicAWSCredentials credential() {
