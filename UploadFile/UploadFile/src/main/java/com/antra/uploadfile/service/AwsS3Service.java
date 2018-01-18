ad5ebbf27658081c6e843e8ecebb79382bee2209
Chaosss <liushashiwr@gmail.com>
2018-01-17 21:52:48 -0500

    delete

--- UploadFile/UploadFile/src/main/java/com/antra/uploadfile/service/AwsS3Service.java
+++
@@ -1,8 +0,0 @@
-package com.antra.uploadfile.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface AwsS3Service {
-
-	public void uploadFile(MultipartFile file);
-}
